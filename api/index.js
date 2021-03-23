dd4342407006f2fa4cfb5484d240edaad08f7959
alan <alanarostegui@hotmail.com>
2021-03-22 21:59:06 -0600

    verifying signed jwt

--- api/index.js
+++ api/index.js
@@ -13,7 +13,21 @@ app.use(express.json());
 app.post("/api/auth/token", (req, res) => {
   const { email, username, name } = req.body;
   const token = jwt.sign({ sub: username, email, name }, config.authJwtSecret);
-  res.json({ access_token: token })
+  res.json({ access_token: token });
+});
+
+app.get("/api/auth/verify", (req, res, next) => {
+  const { access_token } = req.query;
+
+  try {
+    const decoded = jwt.verify(access_token, config.authJwtSecret);
+    res.json({
+      message: "the access token is valid",
+      username: decoded.sub,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 const server = app.listen(5000, () => {
