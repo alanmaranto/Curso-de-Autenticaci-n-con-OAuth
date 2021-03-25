96937e8ac08e541f5db77f698a917fc58059829d
alan <alanarostegui@hotmail.com>
2021-03-25 16:23:16 -0600

    implementando client credentials grant api)

---
+++ api/actions/userActions.js
@@ -0,0 +1,28 @@
+const request = require("request");
+
+function getUserPlaylists(accessToken, userId) {
+  if (!accessToken || !userId) {
+    return Promise.resolve(null);
+  }
+
+  const options = {
+    url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+    headers: { Authorization: `Bearer ${accessToken}` },
+    json: true,
+  };
+
+  return new Promise((resolve, reject) => {
+    request.get(options, function (error, response, body) {
+      if (error || response.statusCode !== 200) {
+          console.log(error)
+        reject(error);
+      }
+
+      resolve(body);
+    });
+  });
+}
+
+module.exports = {
+  getUserPlaylists,
+};
--- api/index.js
+++ api/index.js
@@ -3,20 +3,22 @@ const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const { config } = require("./config");
+const request = require("request");
+const encodeBasic = require("./utils/encodeBasic");
+const { getUserPlaylists } = require("./actions/userActions");
 
 const app = express();
 
-// prod only 
+// prod only
 /* const corsOptions = {
   origin: "http://yourclientside.con"
 }
 
 app.use(cors(corsOptions)); */
 
-
 // middlewares
 // app.use(bodyParser.json())
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 // endpoints
@@ -38,6 +40,34 @@ app.get("/api/auth/verify", (req, res, next) => {
   } catch (error) {
     next(error);
   }
+});
+
+app.get("/api/playlists", async function (req, res, next) {
+  const { userId } = req.query;
+
+  const authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      Authorization: `Basic ${encodeBasic(
+        config.spotifyClientId,
+        config.spotifyClientSecret
+      )}`,
+    },
+    form: {
+      grant_type: "client_credentials",
+    },
+    json: true,
+  };
+
+  request.post(authOptions, async function (error, response, body) {
+    if (error || response.statusCode !== 200) {
+      next(error);
+    }
+
+    const accessToken = body.access_token;
+    const userPlaylists = await getUserPlaylists(accessToken, userId);
+    res.json({ playlists: userPlaylists });
+  });
 });
 
 const server = app.listen(5000, () => {
